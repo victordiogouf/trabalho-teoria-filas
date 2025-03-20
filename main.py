242d14215480515ad9f2099cf48584cc04898a2f
victordiogouf <victor.diogo@estudante.ufjf.br>
2025-03-20 19:35:31 -0300

    apresentação

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from queue_simulator import QueueSimulator, gen_exponential_time, gen_normal_time
     
-def get_int(prompt: str):
+def get_int(prompt: str, default: int | None = None):
   while True:
     try:
-      return int(input(prompt))
+      return int(input(prompt)) if default is None else int(input(prompt) or default)
     except ValueError:
       print("Valor inválido. Tente novamente.")
 
@@ -16,7 +16,7 @@ def get_float(prompt: str):
 
 def get_service_distribution():
   service_distribution = input("Entre com a distribuição do tempo de serviço ([E]xponencial / [n]ormal): ").lower()
-  if service_distribution == 'n':
+  if service_distribution.startswith("n"):
     mu_mean = get_float("Entre com a média do tempo de serviço: ")
     mu_std_dev = get_float("Entre com o desvio padrão do tempo de serviço: ")
     return (gen_normal_time, (mu_mean, mu_std_dev))
@@ -24,23 +24,23 @@ def get_service_distribution():
     mu_rate = get_float("Entre com a taxa de serviço: ")
     return (gen_exponential_time, (mu_rate,))
 
-def get_len_pop():
-    len_pop = input("Entre com a característica da população ([F]inita / [i]nfinita): ").lower()
-    if len_pop == "i":
-        population_size = 2**31 - 1
-    else:
-        population_size = get_int("Entre com o tamanho da população: ")
-    return population_size
-        
+def get_finite_infinite(prompt_a: str, prompt_b: str):
+  len_pop = input(prompt_a).lower()
+  if len_pop.startswith("f"):
+    population_size = get_int(prompt_b)
+  else:
+    population_size = -1
+  return population_size
   
 def main():
   lambda_rate = get_float("Entre com a taxa de chegada: ")
   service_distribution = get_service_distribution()
-  num_servers = get_int("Entre com o número de servidores: ")
-  max_users = get_int("Entre com o número máximo de usuários no sistema: ")
-  population_size = get_len_pop()
+  num_servers = get_int("Entre com o número de servidores (1): ", 1)
+  max_users = get_finite_infinite("Entre com a característica do número máximo de usuários no sistema ([I]nfinita / [f]inita): ", "Entre com o número máimo de usuários no sistema: ")
+  population_size = get_finite_infinite("Entre com a característica da população ([I]nfinita / [f]inita): ", "Entre com o tamanho da população: ")
   simulation_time = get_float("Entre com o tempo total de simulação: ")
 
-  QueueSimulator(lambda_rate, *service_distribution, num_servers, max_users, population_size, simulation_time)
+  simulator = QueueSimulator(lambda_rate, *service_distribution, num_servers, max_users, population_size, simulation_time)
+  simulator.simulate()
 
 main()
--- queue_simulator.py
+++ queue_simulator.py
@@ -2,6 +2,8 @@ from random import random
 from math import sqrt, cos, log, pi
 
 def gen_exponential_time(rate: float):
+  if (rate == 0.0):
+    return float('inf')
   return -log(random()) / rate
 
 def gen_normal_time(mean: float, std_dev: float):
@@ -21,87 +23,84 @@ class QueueSimulator:
     self.gen_service_args = gen_service_args
     self.num_servers = num_servers
     self.max_users = max_users
-    self.population_size = population_size # População disponível
-    self.N = population_size # População total
-    self.time = 0
+    self.population_size = population_size
+    self.simulation_time = simulation_time
+    self.time = 0.0
     self.queue: list[float] = []
-    self.next_arrival = gen_exponential_time(lambda_rate)
+    self.next_arrival = gen_exponential_time(self.lambda_n())
     self.next_departures: list[float] = [float('inf')]
     self.servers_busy = 0
-    self.simulation_time = simulation_time
-    self.file = open("output.txt", "w")
-
+    self.file = open('output.txt', 'w', encoding='utf-8')
+    self.last_arrival = 0.0
+    # Variables for statistics
     self.total_entries = 0
     self.total_service_users = 0.0
     self.total_queue_users = 0.0
     self.total_queue_time = 0.0
-    self.total_service_time = 0.0
+    self.arrival_differences: list[float] = []
+    self.service_times: list[float] = []
 
-    self.arrival_differences = []
-    self.service_times = []
-    self.last_arrival = None
-    
-    self.simulate()
+  def __del__(self):
     self.file.close()
 
   def simulate(self):
     while self.time < self.simulation_time:
       if self.next_arrival < self.next_departures[0]:
-        # Arrival event
-        self.total_service_users += self.servers_busy * (self.next_arrival - self.time)
-        self.total_queue_users += len(self.queue) * (self.next_arrival - self.time)
-
-        self.time = self.next_arrival
-
-        if self.last_arrival is not None:
-          self.arrival_differences.append(self.time - self.last_arrival)
-        else:
-          self.arrival_differences.append(self.time)
-        self.last_arrival = self.time
-        
-        if len(self.queue) + self.servers_busy == self.max_users or self.population_size == 0:
-          self.file.write(f"Usuario rejeitado em {self.time:.5f}\n")
-          self.next_arrival = self.time + gen_exponential_time(self.lambda_rate)
-          continue
-
-        self.file.write(f"Usuario chega em {self.time:.5f}\n")
-        self.population_size -= 1
-        self.total_entries += 1
-        if self.servers_busy < self.num_servers:
-          self.file.write(f"Usuario entra em servico em {self.time:.5f}\n")
-          self.servers_busy += 1
-          service_time = self.gen_service_time(*self.gen_service_args)
-          self.service_times.append(service_time)
-          self.total_service_time += service_time
-          self.insert_departure(self.time + service_time)
-        else:
-          self.queue.append(self.time)
-          
-          if self.N != 2**31-1:
-            self.next_arrival = self.time + gen_exponential_time((self.N-len(self.queue))*self.lambda_rate)
-          else:
-            self.next_arrival = self.time + gen_exponential_time(self.lambda_rate)
+        self.handle_arrival()
       else:
-        # Departure event
-        next_departure = self.next_departures.pop(0)
-        self.total_service_users += self.servers_busy * (next_departure - self.time)
-        self.total_queue_users += len(self.queue) * (next_departure - self.time)
-
-        self.time = next_departure
-        self.file.write(f"Usuario sai em {self.time:.5f}\n")
-        self.population_size += 1
-        if self.queue:
-          self.file.write(f"Usuario entra em servico em {self.time:.5f}\n")
-          arrival_time = self.queue.pop(0)
-          self.total_queue_time += self.time - arrival_time
-          service_time = self.gen_service_time(*self.gen_service_args)
-          self.service_times.append(service_time)
-          self.total_service_time += service_time
-          self.insert_departure(self.time + service_time)
-        else:
-          self.servers_busy -= 1
+        self.handle_departure()
     
     self.print_results()
+
+  def handle_arrival(self):
+    self.total_service_users += self.servers_busy * (self.next_arrival - self.time)
+    self.total_queue_users += len(self.queue) * (self.next_arrival - self.time)
+
+    self.time = self.next_arrival
+    arrival_diff = self.time - self.last_arrival
+    self.arrival_differences.append(arrival_diff)
+    self.last_arrival = self.time
+
+    if self.is_full() or self.population_size == 0:
+      self.file.write(f'{'Usuário rejeitado em':<27}{self.time:>15.5f}\n')
+      self.next_arrival = self.time + gen_exponential_time(self.lambda_n())
+      return
+
+    self.file.write(f'{'Usuário chega em':<27}{self.time:>15.5f}\n')
+    self.population_size -= 1
+    self.total_entries += 1
+    if self.servers_busy < self.num_servers:
+      self.file.write(f'{'Usuário entra em serviço em':<27}{self.time:>15.5f}\n')
+      self.servers_busy += 1
+      service_time = self.gen_service_time(*self.gen_service_args)
+      self.service_times.append(service_time)
+      self.insert_departure(self.time + service_time)
+    else:
+      self.queue.append(self.time)
+      
+    self.next_arrival = self.time + gen_exponential_time(self.lambda_n())
+
+  def handle_departure(self):
+    next_departure = self.next_departures.pop(0)
+    self.total_service_users += self.servers_busy * (next_departure - self.time)
+    self.total_queue_users += len(self.queue) * (next_departure - self.time)
+
+    self.time = next_departure
+    self.file.write(f'{'Usuário sai em':<27}{self.time:>15.5f}\n')
+    self.population_size += 1
+
+    if self.next_arrival == float('inf'):
+      self.next_arrival = self.time + gen_exponential_time(self.lambda_n())
+
+    if self.queue:
+      self.file.write(f'{'Usuário entra em serviço em':<27}{self.time:>15.5f}\n')
+      arrival_time = self.queue.pop(0)
+      self.total_queue_time += self.time - arrival_time
+      service_time = self.gen_service_time(*self.gen_service_args)
+      self.service_times.append(service_time)
+      self.insert_departure(self.time + service_time)
+    else:
+      self.servers_busy -= 1
   
   def insert_departure(self, dep: float):
     for i, departure_time in enumerate(self.next_departures):
@@ -109,11 +108,19 @@ class QueueSimulator:
         self.next_departures.insert(i, dep)
         break
 
+  def lambda_n(self):
+    return self.lambda_rate if self.population_size < 0 else self.population_size * self.lambda_rate
+  
+  def is_full(self):
+    if self.max_users == -1:
+      return False
+    return len(self.queue) + self.servers_busy == self.max_users
+
   def avg_queue_time(self):
     return self.total_queue_time / self.total_entries
   
   def avg_service_time(self):
-    return self.total_service_time / self.total_entries
+    return sum(self.service_times) / len(self.service_times)
   
   def avg_system_time(self):
     return self.avg_queue_time() + self.avg_service_time()
@@ -128,27 +135,25 @@ class QueueSimulator:
     return self.avg_queue_users() + self.avg_service_users()
 
   def avg_arrival_differences(self):
-    return sum(self.arrival_differences)/len(self.arrival_differences)
+    return sum(self.arrival_differences) / len(self.arrival_differences)
   
   def var_arrival_differences(self):
-    return sum((x - self.avg_arrival_differences()) ** 2 for x in self.arrival_differences)/(len(self.arrival_differences)-1)
+    avg = self.avg_arrival_differences()
+    return sum((x - avg) ** 2 for x in self.arrival_differences) / (len(self.arrival_differences) - 1)
 
   def var_service_times(self):
-    return sum((x - self.avg_service_time()) ** 2 for x in self.service_times)/(len(self.service_times)-1)
-
-  def avg_service_times(self):
-    return sum(self.service_times)/len(self.service_times)
+    avg = self.avg_service_time()
+    return sum((x - avg) ** 2 for x in self.service_times) / (len(self.service_times) - 1)
   
   def print_results(self):
-    print("\nResultados da simulação")
-    print("> Tempo médio na fila:", self.avg_queue_time())
-    print("> Tempo médio em serviço:", self.avg_service_time())
-    print("> Tempo médio em serviço TESTE:", self.avg_service_times())
-    print("> Variância dos tempos em serviço:", self.var_service_times())
-    print("> Tempo médio no sistema:", self.avg_system_time())
-    print("> Tempo médio do tempo entre chegadas:", self.avg_arrival_differences())
-    print("> Variância do tempo entre chegadas:", self.var_arrival_differences())
-    print("> Número médio de usuários na fila:", self.avg_queue_users())
-    print("> Número médio de usuários em serviço:", self.avg_service_users())
-    print("> Número médio de usuários no sistema:", self.avg_system_users())
+    print('\nResultados da simulação')
+    print(f'{'-> Tempo médio na fila:':<39}',                 '{:>12.5f}'.format(self.avg_queue_time()))
+    print(f'{'-> Tempo médio em serviço:':<39}',              '{:>12.5f}'.format(self.avg_service_time()))
+    print(f'{'-> Variância dos tempos em serviço:':<39}',     '{:>12.5f}'.format(self.var_service_times()))
+    print(f'{'-> Tempo médio no sistema:':<39}',              '{:>12.5f}'.format(self.avg_system_time()))
+    print(f'{'-> Tempo médio entre chegadas:':<39}',          '{:>12.5f}'.format(self.avg_arrival_differences()))
+    print(f'{'-> Variância do tempo entre chegadas:':<39}',   '{:>12.5f}'.format(self.var_arrival_differences()))
+    print(f'{'-> Número médio de usuários na fila:':<39}',    '{:>12.5f}'.format(self.avg_queue_users()))
+    print(f'{'-> Número médio de usuários em serviço:':<39}', '{:>12.5f}'.format(self.avg_service_users()))
+    print(f'{'-> Número médio de usuários no sistema:':<39}', '{:>12.5f}'.format(self.avg_system_users()))
   
